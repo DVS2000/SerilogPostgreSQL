0dcd22721f76211ae5c4ec727ce4e159f5a1973a
dvs2000 <dorivaldodossantos2000@gmail.com>
2023-05-03 19:01:41 +0100

    Deploy in github

---
+++ SeriLogs.API/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configuração do Serilog com PostgreSQL
+builder.Host.UseSerilog((hostContext, services, configuration) =>
+{
+    var connectionString = hostContext.Configuration.GetConnectionString("serilogCs");
+    configuration
+        .Enrich.FromLogContext()
+        .WriteTo.PostgreSQL(connectionString, "Logs", needAutoCreateTable: true);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
